1d6bd04b26022e693190fb8eaca110d3097408c5
MarieDeSmedt <desmedtmarie@outlook.com>
2021-06-17 14:58:20 +0200

    add to gitignore conf.connect

--- conf/connect.py
+++ conf/connect.py
@@ -1,4 +1,4 @@
-mysql_host="localhost"
-mysql_user="marie"
-mysql_password="marikiki9283"
-database_name="secret_diary"
+mysql_host = "localhost"
+mysql_user = "marie"
+mysql_password = "marikiki9283"
+database_name = "secret_diary"
