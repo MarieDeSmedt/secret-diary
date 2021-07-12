1338504c2554c8fc59c26ee208cb551676b5e7fc
MarieDeSmedt <desmedtmarie@outlook.com>
2021-07-12 16:55:47 +0200

    commit test_api2

--- test/test_api.py
+++ test/test_api.py
@@ -7,6 +7,8 @@ def test_api_get_cust_by_name(session_make):
     assert isinstance(response, model.Customer)
 
 
+
+
 def test_api_get_cust_by_id(session_make):
     response = api.get_cust_by_id(1, session_make)
     assert isinstance(response, model.Customer)
@@ -19,10 +21,36 @@ def test_api_get_all_cust(session_make):
 
 
 def test_api_create_cust(session_make):
-    new_cust = {'name': 'test5',
-                'firstname': 'test52',
-                'information': 'test522',
+    new_cust = {'name': 'test',
+                'firstname': 'test',
+                'information': 'test',
                 'creation_date': '12/12/1212'}
     customer = api.create_customer(new_cust, session_make)
     assert isinstance(customer, model.Customer)
-    assert customer.name == 'test5'
+    assert customer.name == 'test'
+    assert customer.firstname == 'test'
+    assert customer.information == 'test'
+
+
+def test_api_update_cust(session_make):
+    old_cust = api.get_cust_by_name("test", session_make)
+    assert isinstance(old_cust,model.Customer)
+    new_cust = {'name': 'up_test',
+                'firstname': 'up_test',
+                'information': 'up_test',
+                'modification_date': '12/12/1212'}
+    customer = api.update_cust_by_id(old_cust.id_customer,new_cust,session_make)
+    assert isinstance(customer, model.Customer)
+    assert customer.name == 'up_test'
+    assert customer.firstname == 'up_test'
+    assert customer.information == 'up_test'
+
+
+def test_api_delete_cust(session_make):
+    customer_to_delete = api.get_cust_by_name('up_test', session_make)
+    assert isinstance(customer_to_delete,model.Customer)
+    if customer_to_delete:
+        response = api.delete_cust_by_id(customer_to_delete.id_customer,session_make)
+    else:
+        response ="not success"
+    assert response == "Successfully deleted"
